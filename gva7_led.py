1852368f89df83c14c1feef95c2e11c5b3de35e3
TechMakerAI <154943877+techmakerai@users.noreply.github.com>
2024-10-19 18:59:26 -0400

    Update gva7_led.py

--- gva7_led.py
+++ gva7_led.py
@@ -33,7 +33,7 @@ mixer.pre_init(frequency=24000, buffer=2048)
 mixer.init()
 
 # add your Google Gemini API key here
-my_api_key = "  "
+my_api_key = " "
 
 if len(my_api_key) < 5:
     print(f"Please add your Google Gemini API key in the program. \n " )
